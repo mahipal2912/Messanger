c35c40e34792e6c71526142b3b36b26a6ba0cabf
shubhamkodes <shubtomar33@gmail.com>
2023-12-29 15:45:51 +0530

    - Lifecycle Handling - Intent to send mail

--- app/src/main/java/com/hfad/messanger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/messanger/CreateMessageActivity.java
@@ -1,35 +1,79 @@
 package com.hfad.messanger;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.hfad.messanger.databinding.ActivityCreateMessageBinding;
 
 public class CreateMessageActivity extends AppCompatActivity {
+    ActivityCreateMessageBinding binding;
+
+    public static String TAG = "CreateMessageActivity: Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ActivityCreateMessageBinding binding;
-
         super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate: ");
 
         binding = ActivityCreateMessageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         binding.btnSend.setOnClickListener(view -> {
 
-             String mesaageText = binding.etMsg.getText().toString();
+            String title = binding.etTitle.getText().toString().trim();
+            String message = binding.etMsg.getText().toString().trim();
 
-            Intent intent;
-
+            if (title.isEmpty()) {
+                Toast.makeText(this, "Title is empty!!", Toast.LENGTH_SHORT).show();
+            } else if (message.isEmpty()) {
+                Toast.makeText(this, "Message is empty!!", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent;
                 intent = new Intent(this, RecieveMessageActivity.class);
-
-
-            intent.putExtra(RecieveMessageActivity.EXTRA_MESSAGE,mesaageText);
-            startActivity(intent);
+                intent.putExtra(RecieveMessageActivity.EXTRA_MESSAGE, message);
+                intent.putExtra(RecieveMessageActivity.EXTRA_TITLE, title);
+                startActivity(intent);
+            }
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+    }
 }
--- app/src/main/java/com/hfad/messanger/RecieveMessageActivity.java
+++ app/src/main/java/com/hfad/messanger/RecieveMessageActivity.java
@@ -1,42 +1,108 @@
 package com.hfad.messanger;
 
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
 import com.hfad.messanger.databinding.ActivityRecieveMessageBinding;
+
+import java.util.ArrayList;
 
 public class RecieveMessageActivity extends AppCompatActivity {
 
-    public static final  String EXTRA_MESSAGE = "message";
+    private ActivityRecieveMessageBinding binding;
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    public static final String EXTRA_TITLE = "title";
+
+    public static String TAG = "RecieveMessageActivity : Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-         ActivityRecieveMessageBinding binding;
-
         super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate");
         binding = ActivityRecieveMessageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         Intent intent = getIntent();
 
+        String title = intent.getStringExtra(EXTRA_TITLE);
         String messageText = intent.getStringExtra(EXTRA_MESSAGE);
 
+        binding.tvMessage.setText(messageText);
+        binding.tvTitle.setText(title);
 
-        binding.message.setText(messageText);
-
-
-        binding.btnSendto.setOnClickListener(view -> {
-
-            Intent intent1 = new Intent(Intent.ACTION_SEND);
-            intent1.putExtra(Intent.EXTRA_TEXT, messageText);
-            intent1.setType("text/plain");
-
-            Intent chooser = Intent.createChooser(intent1,"send your message to others");
-
-            startActivity(chooser);
+        binding.btnSendtoOthers.setOnClickListener(view -> {
+            composeMessageToOthers(title, messageText);
         });
+
+        binding.btnSendtoMail.setOnClickListener((v) -> {
+            composeEmail(new ArrayList<>(), title, messageText);
+        });
+
     }
 
+    public void composeMessageToOthers(String title, String message) {
+        String fullMessage = title + "\n" + message;
+        Intent intent1 = new Intent(Intent.ACTION_SEND);
+        intent1.putExtra(Intent.EXTRA_TEXT, fullMessage);
+        intent1.setType("text/plain");
+        Intent chooser = Intent.createChooser(intent1, "Send your message to others");
+        startActivity(chooser);
+    }
 
+    public void composeEmail(ArrayList<String> addresses, String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Your device hasn't email app.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+    }
 }
